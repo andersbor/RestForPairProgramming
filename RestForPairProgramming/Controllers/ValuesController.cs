3880f88fdf233f0867b52147b6d737e36b4aa25e
Anders Børjesson <anbo@zealand.dk>
2020-03-23 09:18:41 +0100

    one more option

--- RestForPairProgramming/Controllers/ValuesController.cs
+++ RestForPairProgramming/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace RestForPairProgramming.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "another value" };
         }
 
         // GET api/values/5
